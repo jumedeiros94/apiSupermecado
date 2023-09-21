76b44bc29aab3d7657d61a2fe15bb4e94a02b7f3
Juliana Medeiros <juliana.medeiros@zup.com.br>
2023-09-21 12:08:04 -0300

    subindo front

---
+++ FrontEnd/FrontEnd/js/usuario.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", event =>{
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    fetch("http://localhost:8080/api/produtos", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json()).then(data => console.log(data)).catch(error => console.log(error));
+});
+
+function mensagem(){
+    alert("Cadastro feito com sucesso!")
+    window.location.reload(true);
+}
+
+
+fetch("http://localhost:8080/api/produtos").then((data)=>{
+    return data.json();
+  }).then((todosUsuarios) =>{
+      let data1 = "";
+      todosUsuarios.map((values) =>{
+          data1 += `
+          <tbody>
+              <tr>
+                  <th scope="row">${values.id} </td>
+                  <td>${values.nome}</td> 
+                  <td>${values.preco}</td> 
+                  <td>${values.quantidade} </td>
+              </tr>
+          </tbody>
+          `
+      })
+      document.getElementById("usuarioDados").innerHTML = data1;
+      console.log(todosUsuarios);
+  })
+
+
+
